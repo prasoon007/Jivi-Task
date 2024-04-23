cb0b2126c14b6cd4d518181e96b850ef7d5f52ff
Prasoon Shukla <prasoon.shukla@squareboat.com>
2024-04-23 12:48:03 +0530

    FIX: slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -19,7 +19,7 @@ const RangeSlider = ({ value, setValue, id, min = "60", max = "120" }: RangeProp
                 onChange={(e) => {
                     setValue(e.target.value, id)
                 }}
-                className="bg-gray-300 w-full rounded-full outline-none appearance-none bg-transparent [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-red-500/25 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[24px] [&::-webkit-slider-thumb]:w-[24px] [&::-webkit-slider-thumb]:rounded-lg [&::-webkit-slider-thumb]:bg-blue-500"
+                className="bg-gray-300 w-full rounded-full outline-none appearance-none bg-transparent"
             />
             <div className="flex justify-between mt-2 text-sm text-[#5D6A85]">
                 <span className="w-8 text-left">{"<=" + min}</span>
